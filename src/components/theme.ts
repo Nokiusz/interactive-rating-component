8ce828617a2c84f563ace7a84672428db8914a1c
Mateusz Kłakus <mati.klakus@gmail.com>
2023-02-28 20:18:08 +0100

    feat: theme object

---
+++ src/components/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  lessOrange: 'hsl(25, 56%, 70%)',
+  orange: 'hsl(25, 97%, 53%)',
+  white: 'hsl(0, 0%, 100%)',
+  lightGrey: 'hsl(217, 12%, 63%)',
+  mediumGrey: 'hsl(216, 12%, 54%)',
+  lessDarkBlue: 'hsl(213, 19%, 25%)',
+  darkBlue: 'hsl(213, 19%, 18%)',
+  darkerBlue: 'hsl(213, 19%, 7%)',
+  veryDarkBlue: 'hsl(216, 12%, 8%)',
+  fontSize: '15px',
+  mobile: '375px',
+  desktop: '1440px',
+};
+
+export declare type Theme = typeof theme;
