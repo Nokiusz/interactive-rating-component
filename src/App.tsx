d10c247d2ac4434470bdfa05591b3777ec470ea1
Mateusz Kłakus <mati.klakus@gmail.com>
2023-02-28 20:17:48 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { Rating } from './components';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Rating />
+    </div>
+  );
+}
+
+export default App;
